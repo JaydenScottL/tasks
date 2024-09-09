3e6ebec63fcff41a2ee7c9e72e3be2ed323708c3
Jayden LeCorps <jayden.lecorps@maine.edu>
2024-09-09 14:34:58 -0400

    Edited App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,9 @@ function App(): React.JSX.Element {
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
             </p>
+
+            <b>Jayden LeCorps</b>
+            <ul>COS 420 test</ul>
         </div>
     );
 }
