ac1bd74b8003a67049ff2f237d30540a5b1495bd
Jayden LeCorps <jayden.lecorps@maine.edu>
2024-11-13 20:07:04 -0500

    Merge

--- src/bad-components/ColoredBox.tsx
+++ src/bad-components/ColoredBox.tsx
@@ -4,12 +4,17 @@ import { Button } from "react-bootstrap";
 export const COLORS = ["red", "blue", "green"];
 const DEFAULT_COLOR_INDEX = 0;
 
-function ChangeColor(): React.JSX.Element {
-    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
+function ChangeColor({
+    value,
+    access,
+}: {
+    value: number;
+    access: (newValue: number) => void;
+}): React.JSX.Element {
     return (
         <Button
             onClick={() => {
-                setColorIndex((1 + colorIndex) % COLORS.length);
+                access((1 + value) % COLORS.length);
             }}
         >
             Next Color
@@ -17,14 +22,14 @@ function ChangeColor(): React.JSX.Element {
     );
 }
 
-function ColorPreview(): React.JSX.Element {
+function ColorPreview({ value }: { value: number }): React.JSX.Element {
     return (
         <div
             data-testid="colored-box"
             style={{
                 width: "50px",
                 height: "50px",
-                backgroundColor: COLORS[DEFAULT_COLOR_INDEX],
+                backgroundColor: COLORS[value],
                 display: "inline-block",
                 verticalAlign: "bottom",
                 marginLeft: "5px",
@@ -34,13 +39,17 @@ function ColorPreview(): React.JSX.Element {
 }
 
 export function ColoredBox(): React.JSX.Element {
+    const [colorIndex, setColorIndex] = useState<number>(DEFAULT_COLOR_INDEX);
     return (
         <div>
             <h3>Colored Box</h3>
             <span>The current color is: {COLORS[DEFAULT_COLOR_INDEX]}</span>
             <div>
-                <ChangeColor></ChangeColor>
-                <ColorPreview></ColorPreview>
+                <ChangeColor
+                    value={colorIndex}
+                    access={setColorIndex}
+                ></ChangeColor>
+                <ColorPreview value={colorIndex}></ColorPreview>
             </div>
         </div>
     );
